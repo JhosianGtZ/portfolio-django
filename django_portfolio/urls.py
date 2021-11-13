4df455f13ec342e8fd3399bf5d69b77fee7f81a8
Jhosian <jhosianlpz@gmail.com>
2021-11-12 22:16:41 -0600

    Responsive design 99% , Animations and SEO Tags Unsolved, README at 80%

--- django_portfolio/urls.py
+++ django_portfolio/urls.py
@@ -25,8 +25,9 @@ from portfolio import views
 
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    
     path("", views.home, name="home"),
+    path("admin/", admin.site.urls),
     path("blog/", include('blog.urls')),
 ]
 
